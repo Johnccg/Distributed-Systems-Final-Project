0fd2d2d3873a0f5e0814b6b46bfb527a027ef9f0
Juan Carlos Calderón <150750774+Johnccg@users.noreply.github.com>
2026-01-18 23:54:23 +0700

    Allow client to switch servers

--- client/client.js
+++ client/client.js
@@ -1,32 +1,50 @@
 const senderInput = document.getElementById("sender");
 const roomInput = document.getElementById("room");
+const serverInput = document.getElementById("server");
 const messageInput = document.getElementById("message");
 const chat = document.getElementById("chat");
-const ws = new WebSocket("ws://localhost:3000");
+const sendBtn = document.getElementById("send");
 
-ws.onopen = () => {
-    chat.textContent += "[connected]\n"
-};
+const port = serverInput.value || 3000;
 
-ws.onmessage = event => {
-    try {
-        const msg = JSON.parse(event.data);
-        chat.textContent += `${msg.sender}@${msg.room}: ${msg.content}\n`
-    } catch {
-        chat.textContent += event.data + "\n"
-    }
-};
+function createWS(port) {
+    const ws = new WebSocket(`ws://localhost:${port}`);
 
-ws.onclose = () => {
-    chat.textContent += "[disconnected]\n"
-};
-
-send.onclick = () => {
-    const payload = {
-        sender: senderInput.value || "anon",
-        room: roomInput.value || "general",
-        content: messageInput.value
+    ws.onopen = () => {
+        chat.textContent += `[connected ${port}]\n`
     };
-    ws.send(JSON.stringify(payload));
-    messageInput.value = "";
-};
+
+    ws.onmessage = event => {
+        try {
+            const msg = JSON.parse(event.data);
+            chat.textContent += `${msg.sender}@${msg.room}: ${msg.content}\n`
+        } catch {
+            chat.textContent += event.data + "\n"
+        }
+    };
+
+    ws.onclose = () => {
+        chat.textContent += "[disconnected]\n"
+    };
+
+    sendBtn.onclick = () => {
+        const payload = {
+            sender: senderInput.value || "anon",
+            room: "general",
+            // room: roomInput.value || "general",
+            content: messageInput.value
+        };
+        ws.send(JSON.stringify(payload));
+        messageInput.value = "";
+    };
+
+    return ws;
+}
+
+let ws = createWS(port);
+
+roomInput.onchange = () => {
+    ws.close();
+    const newPort = serverInput.value || 3000;
+    ws = createWS(newPort);
+}
